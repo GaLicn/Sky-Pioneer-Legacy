21ed44a39efbb10cde497919028776367c2d6bea
GaLicn <133291877+GaLicn@users.noreply.github.com>
2026-02-07 01:23:32 +0800

    草木灰

--- src/main/java/com/gali/sky_pioneer_legacy/item/ModItems.java
+++ src/main/java/com/gali/sky_pioneer_legacy/item/ModItems.java
@@ -2,6 +2,7 @@ package com.gali.sky_pioneer_legacy.item;
 
 import com.gali.sky_pioneer_legacy.Sky_pioneer_legacy;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.BoneMealItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ShearsItem;
 import net.neoforged.bus.api.IEventBus;
@@ -14,6 +15,9 @@ public class ModItems {
 
     public static final Supplier<Item> WOODEN_SHEARS = ITEMS.register("wooden_shears",
             () -> new ShearsItem(new Item.Properties().durability(32)));
+
+    public static final Supplier<Item> GRASS_ASH = ITEMS.register("grass_ash",
+            () -> new BoneMealItem(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
